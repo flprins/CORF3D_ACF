5cdc6306a7dc6eb8038dcf72bf4b29f35eab4808
ameybhole <amey.bhole77@gmail.com>
2021-10-03 18:24:57 +0530

    Added CORFpushpull with preprocessing (test)

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,10 @@
 import matlab.engine
-from imageio import imread
+import matplotlib.pyplot as plt
 
 m = matlab.engine.start_matlab()
-m.CORFContourDetection(imread('D:\Amey\Masters\Projects\CORF3D_HCR\data\processed'
-                               '\Preprocessed_RGB\0086\0086_1_1.jpg'), 2.2,4,1.8,0.005)
+m.addpath('.\CORFpushpull', nargout=0)
+data = m.imread('D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB\\0086\\0086_1_1.jpg')
+binarymap, corfresponse = m.CORFContourDetection(data, 2.2, 4, 1.8, 0.005, nargout=2)
+
+plt.imshow(corfresponse)
+plt.show()
