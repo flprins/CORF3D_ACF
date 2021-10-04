151da9f2c25aa9ac0624bbc72875ba7fbee8844c
ameybhole <amey.bhole77@gmail.com>
2021-10-04 23:20:10 +0530

    Batch data_preprocessing (test)

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,10 +1,28 @@
 import matlab.engine
+import numpy as np
+import os
+
 import matplotlib.pyplot as plt
 
 m = matlab.engine.start_matlab()
 m.addpath('.\CORFpushpull', nargout=0)
-data = m.imread('D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB\\0086\\0086_1_1.jpg')
-binarymap, corfresponse = m.CORFContourDetection(data, 2.2, 4, 1.8, 0.005, nargout=2)
 
-plt.imshow(corfresponse)
-plt.show()
+img_data_list = []
+labels = []
+list_of_image_paths = []
+labels_list = []
+
+data_dir_list = os.listdir("D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB")
+for folder_name in data_dir_list:
+    img_list = os.listdir("D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB" + '/' + folder_name)
+    for image in img_list:
+        retrieve_dir = "D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB" + "/" + folder_name + "/" + image
+        print("Reading image"+retrieve_dir)
+        images = m.imread(retrieve_dir)
+        binarymap, corfresponse = m.CORFContourDetection(images, 2.2, 4, 1.8, 0.005, nargout=2)
+        list_of_image_paths.append(corfresponse)
+        print(np.array(list_of_image_paths).shape)
+        plt.imshow(corfresponse)
+        plt.show()
+
+
