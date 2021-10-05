945cbe014784a14f4a5b3be4ad21ab0b197aa51e
ameybhole <amey.bhole77@gmail.com>
2021-10-05 08:31:48 +0530

    Updated CORF feature map function

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,14 +6,22 @@ m = matlab.engine.start_matlab()
 m.addpath('.\CORFpushpull', nargout=0)
 
 
-def corf_feature_maps(dataset):
+def corf_feature_maps(dataset, sigma, beta, inhibitionFactor, highthresh ):
 
     """
 
-    Function to return an list of images and its labels
+    Function to return an list of CORF feature maps
 
-    :param dataset: Folder with class name and all the images in the dataset
-    :return: Lists of CORF feature maps in an array form
+    :param dataset: Folder with class name and all the pre-processed images
+    :param sigma: The standard deviation of the DoG functions used
+    :param beta: The increase in the distance between the sets of center-on
+                 and ceter-off DoG receptive fields
+    :param inhibitionFactor: The factor by which the response exhibited in the
+                             inhibitory receptive field suppresses the response exhibited in the
+                             excitatory receptive field
+    :param highthresh: The high threshold of the non-maximum suppression used for binarization
+
+    :return: Lists of the response map of the rotation-invariant CORF operator (CORF feature maps) in an array form
 
     """
 
@@ -25,7 +33,9 @@ def corf_feature_maps(dataset):
         for image in img_list:
             retrieve_dir = dataset + "/" + folder_name + "/" + image
             images = m.imread(retrieve_dir)
-            binarymap, corfresponse = m.CORFContourDetection(images, 2.2, 4, 1.8, 0.005, nargout=2)
+            binarymap, corfresponse = m.CORFContourDetection(images, sigma, beta, inhibitionFactor, highthresh, nargout=2)
             list_of_image_paths.append(corfresponse)
 
     return np.array(list_of_image_paths)
+
+
