d2832fcf1e0a5e4a65b8812d5dcadbe48105fb12
ameybhole <amey.bhole77@gmail.com>
2021-10-05 08:24:09 +0530

    Added CORF feature map function

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,27 +2,30 @@ import matlab.engine
 import numpy as np
 import os
 
-import matplotlib.pyplot as plt
-
 m = matlab.engine.start_matlab()
 m.addpath('.\CORFpushpull', nargout=0)
 
-img_data_list = []
-labels = []
-list_of_image_paths = []
-labels_list = []
 
-data_dir_list = os.listdir("D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB")
-for folder_name in data_dir_list:
-    img_list = os.listdir("D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB" + '/' + folder_name)
-    for image in img_list:
-        retrieve_dir = "D:\Amey\Masters\Projects\CORF3D_HCR\data\processed\Preprocessed_RGB" + "/" + folder_name + "/" + image
-        print("Reading image"+retrieve_dir)
-        images = m.imread(retrieve_dir)
-        binarymap, corfresponse = m.CORFContourDetection(images, 2.2, 4, 1.8, 0.005, nargout=2)
-        list_of_image_paths.append(corfresponse)
-        print(np.array(list_of_image_paths).shape)
-        plt.imshow(corfresponse)
-        plt.show()
+def corf_feature_maps(dataset):
 
+    """
 
+    Function to return an list of images and its labels
+
+    :param dataset: Folder with class name and all the images in the dataset
+    :return: Lists of CORF feature maps in an array form
+
+    """
+
+    list_of_image_paths = []
+
+    data_dir_list = os.listdir(dataset)
+    for folder_name in data_dir_list:
+        img_list = os.listdir(dataset + '/' + folder_name)
+        for image in img_list:
+            retrieve_dir = dataset + "/" + folder_name + "/" + image
+            images = m.imread(retrieve_dir)
+            binarymap, corfresponse = m.CORFContourDetection(images, 2.2, 4, 1.8, 0.005, nargout=2)
+            list_of_image_paths.append(corfresponse)
+
+    return np.array(list_of_image_paths)
